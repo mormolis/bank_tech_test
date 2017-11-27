39ddb0e52499ed3a5e5a873727da0ee669d81d3a
mormolis <george.lamprakis@gmail.com>
2017-11-27 13:05:17 +0000

    adds confirm withdrawal method

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,5 @@
 class Account
+  attr_reader :current_balance
   def initialize(user_id, user_pin)
     @transactions = []
     @user_id = user_id
@@ -7,11 +8,28 @@ class Account
   end
 
   def confirm_deposit(amount)
-    if !amount.nil? && (amount.is_a? Numeric) && amount > 0
+    if acceptable_deposit_amount(amount)
       @current_balance += amount
       return true
     end
     false
+  end
+
+  def confirm_withdrawal(amount)
+    if acceptable_withdrawal_amount(amount)
+      @current_balance -= amount
+      return true
+    end
+    false
+  end
+
+  private
+  def acceptable_deposit_amount(amount)
+    !amount.nil? && (amount.is_a? Numeric) && amount > 0
+  end
+
+  def acceptable_withdrawal_amount(amount)
+    acceptable_deposit_amount(amount) && ((current_balance - amount) >= 0)
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,7 +7,7 @@ describe "Account" do
     end
 
     it "should take positive float and return true" do
-      expect(account.confirm_deposit(5.80)).to eq(true)
+      expect(account.confirm_deposit(5.00)).to eq(true)
     end
 
     it "should return false on zero and negative amounts" do\
@@ -18,9 +18,23 @@ describe "Account" do
     it "should return false on object rather than numeric values" do\
       expect(account.confirm_deposit(Object.new)).to eq(false)
     end
+  end
 
+  describe "#confirm_withdrawal " do
 
+    it "should return false if the amount is greater than the current ballance" do
+      expect(account.confirm_withdrawal(100)).to eq(false)
+    end
+    it "should return true if the amount is less than the current ballance" do
+
+      expect(account.confirm_withdrawal(5)).to eq(true)
+    end
+    it "should return true if the amount is equal to the current ballance" do
+
+      expect(account.confirm_withdrawal(5.0)).to eq(true)
+    end
 
   end
+
 
 end
